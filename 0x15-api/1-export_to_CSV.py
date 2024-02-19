2c64c814191fc05ae222027ded4e5a9cf7574939
Samson Wayne <smuhadia4@gmail.com>
2024-02-19 12:50:18 -0800

    modified file 1. it was long

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a specified employee ID to CSV format."""
+"""Exports to-do list information for a given employee ID to CSV format."""
 
 import csv
 import requests
@@ -7,26 +7,28 @@ import sys
 
 
 if __name__ == "__main__":
-    # Extract user ID from the provided command-line arguments
+    # Get the user ID from the command-line arguments provided to the script
     user_id = sys.argv[1]
 
-    # Base URL for the JSON API
-    base_url = "https://jsonplaceholder.typicode.com/"
+    # Define the base URL for the JSON API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetch user data from the API and parse the response as JSON
-    user_data = requests.get(base_url + "users/{}".format(user_id)).json()
+    # Fetch user information from the API and
+    #   convert the response to a JSON object
+    user = requests.get(url + "users/{}".format(user_id)).json()
 
     # Extract the username from the user data
-    username = user_data.get("username")
+    username = user.get("username")
 
-    # Retrieve the to-do list items associated with the user ID and parse the response as JSON
-    todos = requests.get(base_url + "todos", params={"userId": user_id}).json()
+    # Fetch the to-do list items associated with the
+    #   given user ID and convert the response to a JSON object
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # Use list comprehension to iterate through the to-do list items
-    # Write the details of each item (user ID, username, completion status,
+    # Use list comprehension to iterate over the to-do list items
+    # Write each item's details (user ID, username, completion status,
     #   and title) as a row in the CSV file
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, task.get("completed"), task.get("title")]
-         ) for task in todos]
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
