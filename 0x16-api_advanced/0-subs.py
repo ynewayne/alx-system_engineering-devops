6f47c0b17a5d2e172fb33665e6d8354c6ed9974f
Samson Wayne <smuhadia4@gmail.com>
2024-03-12 07:41:17 -0700

    corrected 0 and 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-'''
-    this module contains the function number_of_subscribers
-'''
+***The function retrieves the subscriber count from reddit subreddit.***
 import requests
-from sys import argv
-
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    ***return total number of subscribers from a specific subreddit.***
+    url = "http://www.reddit.com/r/{}about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by u/bdov/_)"
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    results = response.json().get("data")
+    return results.get("subscribers")
