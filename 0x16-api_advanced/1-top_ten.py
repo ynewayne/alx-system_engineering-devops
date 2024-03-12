a221fbe1ccbb398e56c966495f75562e97ed1458
Samson Wayne <smuhadia4@gmail.com>
2024-03-12 08:10:38 -0700

    corrected the 2-recurse.py file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-*** this module contains the function top_ten.***
-import requests
+"""Module for task 1"""
+
 
 def top_ten(subreddit):
-    ***returns the top ten posts for a given subreddit***
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-headers = {"User_Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-        }
-params = {
-        "limit":10
-        }
-response = requests.get(url, headers=headers, params=params,
-        allow_redirects=False)
-if response.status_code == 404:
-    print("None")
-    return
-results = response.json().get("data")
-[print(c.get("data").get("title")) for c in results.get("children")]
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
