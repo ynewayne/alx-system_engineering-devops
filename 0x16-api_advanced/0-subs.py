63385a1c2b8adf60d6fc8c230000e1ce72885367
Samson Wayne <smuhadia4@gmail.com>
2024-03-18 11:21:25 -0700

    added to 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,3 +17,9 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
